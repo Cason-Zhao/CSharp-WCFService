e744da8c6fa337edd505c693466b2d13672513c9
丹墨 <yixideximo@outlook.com>
2022-09-01 22:23:13 +0800

    IIS方式宿主以及测试客户端。

---
+++ WCFDemo01/WCFService.Client2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService.Client2.HelloWorldService;
+
+namespace WCFService.Client2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var proxyClient = new HelloWorldServiceClient())
+            {
+                Console.WriteLine($"服务访问成功：获取结果---》》》{proxyClient.GetHelloWorld()}");
+            }
+
+            Console.Read();
+        }
+    }
+}
