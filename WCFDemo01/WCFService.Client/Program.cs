e719122e8ae3eaef6b92fcde947bbe1fa25cd51f
丹墨 <yixideximo@outlook.com>
2022-09-01 21:26:23 +0800

    创建客户端访问WCF服务。

---
+++ WCFDemo01/WCFService.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService.Client.HelloWorldService;
+
+namespace WCFService.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestWCFService();
+        }
+
+        private static void TestWCFService()
+        {
+            using (var serviceClient = new HelloWorldServiceClient())
+            {
+                Console.WriteLine($"服务访问成功：获取结果---》》》{serviceClient.GetHelloWorld()}");
+            }
+
+            Console.Read();
+        }
+    }
+}
